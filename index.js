70518847da6e8b0c6e00517fd7b86c8a0556d6bd
Balevicky <balevictorien@yahoo.fr>
2025-01-23 16:53:16 +0000

    modified index.html,  style.css and index.js

--- index.js
+++ index.js
@@ -5,3 +5,24 @@
 // S'assurer que les liens sont clickables
 
 // Donner un style de transparence aux 2 plus gros ronds (mix-blend-mode)
+// =================================
+
+// // ================ premiere methode========
+// window.addEventListener("mousemove", (e) => {
+//   cursor.style.left = e.pageX + "px";
+//   cursor.style.top = e.pageY + "px";
+//   mouse1.style.left = e.pageX + "px";
+//   mouse1.style.top = e.pageY + "px";
+//   mouse2.style.left = e.pageX + "px";
+//   mouse2.style.top = e.pageY + "px";
+//   // console.log(mousemove,mousemove1,mousemove2);
+// });
+
+// ================ deuxième methode========
+const mouses = document.querySelectorAll(".mouse");
+window.addEventListener("mousemove", (e) => {
+  mouses.forEach((mouse) => {
+    mouse.style.top = e.y + "px";
+    mouse.style.left = e.x + "px";
+  });
+});
